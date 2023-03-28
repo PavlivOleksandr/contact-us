971b52607aa95a4fc44d30dd6eef8f0da2c0a31e
PAVLIV_OLEKSANDR <dassanya9@gmail.com>
2023-03-28 13:12:42 +0300

    new update

--- server.js
+++ server.js
@@ -2,12 +2,5 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const rootRouter = express.Router();
 
-const buildPath = path.normalize(path.join(__dirname, "../build"));
-app.use(express.static(buildPath));
-
-rootRouter.get("(/*)?", async (req, res, next) => {
-  res.sendFile(path.join(buildPath, "index.html"));
-});
-app.use(rootRouter);
+app.use(express.static(path.join(__dirname, "..", "build")));
